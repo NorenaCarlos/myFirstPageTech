271c54fbe2f4ee23d4fae4aeb464dc700c216444
NorenaCarlos <70341407+NorenaCarlos@users.noreply.github.com>
2024-11-20 11:36:13 -0500

    primer commit de la pagina

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+
+  if (!name || !email) {
+    alert('Por favor, completa todos los campos.');
+    event.preventDefault();
+  } else {
+    alert('Formulario enviado correctamente.');
+  }
+});
